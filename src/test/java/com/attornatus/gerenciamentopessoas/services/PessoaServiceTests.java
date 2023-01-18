df95d3ca97828e233a536e930679526821cd6ae8
akaMiike <mike.nss@alu.ufc.br>
2023-01-18 13:08:48 -0300

    test: adicionada classe de teste para PessoaService

---
+++ src/test/java/com/attornatus/gerenciamentopessoas/services/PessoaServiceTests.java
@@ -0,0 +1,103 @@
+package com.attornatus.gerenciamentopessoas.services;
+
+import com.attornatus.gerenciamentopessoas.entities.Pessoa;
+import com.attornatus.gerenciamentopessoas.repositories.PessoaRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+public class PessoaServiceTests {
+
+    @Autowired
+    private PessoaService pessoaService;
+    @MockBean
+    private PessoaRepository pessoaRepository;
+
+    @Test
+    @DisplayName("QUANDO criar uma pessoa com dados válidos DEVE criar a nova pessoa")
+    public void criarPessoaComDadosValidos(){
+        Pessoa novaPessoa = new Pessoa();
+
+        novaPessoa.setNome("Michael");
+        novaPessoa.setDataNascimento(LocalDate.of(2001,9,28));
+
+        pessoaService.criar(novaPessoa);
+        verify(pessoaRepository,times(1)).save(novaPessoa);
+    }
+
+    @Test
+    @DisplayName("QUANDO atualizar uma pessoa com id válido DEVE atualizar os dados da pessoa")
+    public void atualizarPessoaComIdValido(){
+        Pessoa pessoa = new Pessoa();
+
+        pessoa.setNome("Michael");
+        pessoa.setDataNascimento(LocalDate.of(2001,9,28));
+
+        when(pessoaRepository.existsById(anyInt())).thenReturn(true);
+
+        pessoaService.atualizar(pessoa, 1);
+        verify(pessoaRepository,times(1)).save(pessoa);
+    }
+
+    @Test
+    @DisplayName("QUANDO atualizar uma pessoa com id inválido DEVE retornar exceção")
+    public void atualizarPessoaComIdInvalido(){
+        Pessoa pessoa = new Pessoa();
+
+        pessoa.setNome("Michael");
+        pessoa.setDataNascimento(LocalDate.of(2001,9,28));
+
+        when(pessoaRepository.existsById(anyInt())).thenReturn(false);
+        Assertions.assertThrows(ResponseStatusException.class,
+                () -> pessoaService.atualizar(pessoa, 99)
+        );
+    }
+
+    @Test
+    @DisplayName("QUANDO buscar pessoa por id por uma pessoa existente DEVE retornar a pessoa")
+    public void buscarPessoaPorIdComPessoaExistente(){
+        Pessoa pessoa = new Pessoa();
+
+        pessoa.setId(1);
+        pessoa.setNome("Michael");
+        pessoa.setDataNascimento(LocalDate.of(2001,9,28));
+
+        when(pessoaRepository.findById(anyInt())).thenReturn(Optional.of(pessoa));
+
+        Pessoa pessoaObtida = pessoaService.buscarPorId(1);
+        assertThat(pessoaObtida).isEqualTo(pessoa);
+    }
+
+    @Test
+    @DisplayName("QUANDO buscar pessoa por id por uma pessoa não existente DEVE retornar uma exceção")
+    public void buscarPessoaPorIdComPessoaNaoExistente(){
+        when(pessoaRepository.findById(anyInt())).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(ResponseStatusException.class,
+                () -> pessoaService.buscarPorId(1)
+        );
+    }
+
+    @Test
+    @DisplayName("QUANDO buscar por todas as pessoas DEVE retornar todas as pessoas existentes")
+    public void buscarTodasPessoas(){
+        when(pessoaRepository.findAll()).thenReturn(new ArrayList<>());
+
+        List<Pessoa> pessoas = pessoaService.buscarTodas();
+        assertThat(pessoas).isEqualTo(new ArrayList<>());
+    }
+
+}
